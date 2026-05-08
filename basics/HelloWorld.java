ee5cf6009bc877eebd01fa26bcfdbff45ff3bb82
Jiang <zhou420476@gmail.com>
2026-05-08 19:45:25 +0800

    添加第一个 HelloWorld.java 示例程序

---
+++ basics/HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World! 欢迎来到 Java 学习仓库！");
+        System.out.println("这是你的第一个 Java 程序 🚀");
+    }
+}
